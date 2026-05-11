5506615db9cf9286ef96c9f26d1fbfce619f6203
ThureinWinHtun <thureinwintun@securelinkmm.com>
2026-05-11 21:36:04 +0630

    [Edit] the route structure

--- note-taking-app/src/routes/Router.tsx
+++ note-taking-app/src/routes/Router.tsx
@@ -1,28 +1,30 @@
-import { Route ,Routes,Navigate} from 'react-router-dom'
+import { Route, Routes, Navigate, Outlet } from 'react-router-dom'
 import { HomePage } from '../pages/HomePage'
 import { LoginForm } from '../auth/LoginForm'
 import { SignUpForm } from '../auth/SignUpForm'
 import { CategoriesPage } from '../components/tag_categories/CategoriesPage'
-import type { JSX } from 'react'
 
- const CreateProtectedRouter = ({ children }: {children:JSX.Element}) => {
-
+const AuthenticatedRoutes = () => {
     const isAuthenticated = localStorage.getItem("token");
 
     if (!isAuthenticated) {
         return <Navigate to="/login" replace />
     }
 
-    return children;
+    return <Outlet />;
 };
-export const Router = () =>{
-   
+
+export const Router = () => {
+
     return (
-    <Routes>
-        <Route path='/' element= {<CreateProtectedRouter><HomePage/></CreateProtectedRouter>}/>
-        <Route path='/login' element= {<LoginForm/>}/>
-        <Route path='/signup' element= {<SignUpForm/>}/>
-        <Route path='/category' element= {<CreateProtectedRouter><CategoriesPage/></CreateProtectedRouter>}/>
-    </Routes>
-  )
+        <Routes>
+            <Route path='/login' element={<LoginForm />} />
+            <Route path='/signup' element={<SignUpForm />} />
+
+            <Route element={<AuthenticatedRoutes />}>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/category' element={<CategoriesPage />} />
+            </Route>
+        </Routes>
+    )
 }
