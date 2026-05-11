9fb36e56d2343d7899df77cee01806e0e448479a
ThureinWinHtun <thureinwintun@securelinkmm.com>
2026-05-11 21:44:56 +0630

    [Fix] theme and alignment of log in form

--- note-taking-app/src/App.tsx
+++ note-taking-app/src/App.tsx
@@ -1,9 +1,12 @@
+import { ThemeProvider } from '@mui/material';
 import { Router } from './routes/Router';
+import { theme } from './theme/theme';
+import './App.css';
 
 export default function App() {
   return (
-    <>
-    <Router/>
-    </>
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
   )
 }
--- note-taking-app/src/auth/LoginForm.tsx
+++ note-taking-app/src/auth/LoginForm.tsx
@@ -161,116 +161,106 @@ import {
   Stack,
   Button,
 } from "@mui/material";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import AccountCircle from "@mui/icons-material/AccountCircle";
 import Email from "@mui/icons-material/Email";
 import Lock from "@mui/icons-material/Lock";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#accbed",
-      dark: "#6596c6",
-    },
-  },
-});
+
 
 export const LoginForm = () => {
   const navigate = useNavigate();
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const handleLogin = () => {
-    if (email.trim() === "" || password.trim() === ""){
-      alert ("Fill Email and Password");
+    if (email.trim() === "" || password.trim() === "") {
+      alert("Fill Email and Password");
       return;
     }
     localStorage.setItem("token", "user-login");
     navigate("/");
   };
-  return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "100vh",
-          bgcolor: "primary.main",
-          "&:hover": { bgcolor: "primary.dark", pt: 5 },
-        }}
-      >
-        <Paper elevation={10} sx={{ width: 420, p: 4, borderRadius: 3 }}>
-          <Typography
-            variant="h5"
-            sx={{ textAlign: "center", fontWeight: "bold", color: "#2f72ba" }}
-          >
-            Sign In
-          </Typography>
-          {/* <TextField fullWidth margin='normal' variant='standard' label="Name" slotProps={{input: {startAdornment: (
-                    <InputAdornment position="start"> <AccountCircle /></InputAdornment> ),},}} /> */}
-          <TextField
-            fullWidth
-            margin="normal"
-            variant="standard"
-            label="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    {" "}
-                    <Email />
-                  </InputAdornment>
-                ),
-              },
-            }}
-          />
-          <TextField
-            fullWidth
-            margin="normal"
-            variant="standard"
-            label="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    {" "}
-                    <Lock />
-                  </InputAdornment>
-                ),
-              },
-            }}
-          />
 
-          <Stack
-            spacing={2}
-            direction="row"
-            sx={{ mt: 3, justifyContent: "center", alignItems: "center" }}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        bgcolor: "primary.main",
+        minHeight: "100vh"
+        // "&:hover": { bgcolor: "primary.dark", pt: 5 },
+      }}
+    >
+      <Paper elevation={10} sx={{ width: 420, p: 4, borderRadius: 3 }}>
+        <Typography
+          variant="h5"
+          sx={{ textAlign: "center", fontWeight: "bold", color: "#2f72ba" }}
+        >
+          Sign In
+        </Typography>
+        {/* <TextField fullWidth margin='normal' variant='standard' label="Name" slotProps={{input: {startAdornment: (
+                    <InputAdornment position="start"> <AccountCircle /></InputAdornment> ),},}} /> */}
+        <TextField
+          fullWidth
+          margin="normal"
+          variant="standard"
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  {" "}
+                  <Email />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+        <TextField
+          fullWidth
+          margin="normal"
+          variant="standard"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  {" "}
+                  <Lock />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+
+        <Stack
+          spacing={2}
+          direction="row"
+          sx={{ mt: 3, justifyContent: "center", alignItems: "center" }}
+        >
+          <Button
+            variant="contained"
+            color="info"
+            sx={{
+              mt: 1,
+              py: 1.2,
+              borderRadius: 2,
+              textTransform: "none",
+              fontWeight: "bold",
+            }}
+            onClick={handleLogin}
           >
-            <Button
-              variant="contained"
-              color="info"
-              sx={{
-                mt: 1,
-                py: 1.2,
-                borderRadius: 2,
-                textTransform: "none",
-                fontWeight: "bold",
-              }}
-              onClick={handleLogin}
-            >
-              {" "}
-              Submit
-            </Button>
-            {/* <Button variant="outlined" color='info' sx={{mt:1, py:1.0,borderRadius:2, textTransform:"none",fontWeight:"bold"}}> Sign Out</Button> */}
-          </Stack>
-        </Paper>
-      </Box>
-    </ThemeProvider>
+            {" "}
+            Submit
+          </Button>
+          {/* <Button variant="outlined" color='info' sx={{mt:1, py:1.0,borderRadius:2, textTransform:"none",fontWeight:"bold"}}> Sign Out</Button> */}
+        </Stack>
+      </Paper>
+    </Box>
   );
 };
